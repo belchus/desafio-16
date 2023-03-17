ff3e1ae9d3e4fdb3a8a1180e7fb61085ea3232c9
Belen <arenabelu@gmail.com>
2023-03-16 23:18:11 -0300

    se agrego logger

---
+++ options/options.js
@@ -0,0 +1,16 @@
+const mySql = {
+    client:'mysql',
+    connection: {
+        host:'127.0.0.1',
+        user:'root',
+        password:'',
+        database:'ecommerce',
+    }
+}
+
+
+module.exports = {
+
+    options: mySql
+    
+    }
---
+++ options/options2.js
@@ -0,0 +1,12 @@
+
+const sqlite3 = {
+    client: 'sqlite3',
+    connection: './DB/db.sqlite',
+    useNullAsDefault: true
+}
+
+module.exports = {
+
+    options: sqlite3
+    
+    }
